d3ddaf3c68d685c1b342961f511f56ca28d5884c
Rubén Arrebola de Haro <ruben.arre6@gmail.com>
2019-01-12 21:19:43 +0100

    Completado el modelo, adaptado el panel de administración de django para poder generar datos correctamente y completado el portafolio para tener los datos dinamicos implementando el model MVT para las templates dinamicas

--- 2-Web_Personal/webpersonal/core/views.py
+++ 2-Web_Personal/webpersonal/core/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request, "core/home.html")
 
-def portfolio(request):
-    return render(request, "core/portfolio.html")
-
 def contact(request):
     return render(request, "core/contact.html")
 
--- 2-Web_Personal/webpersonal/webpersonal/urls.py
+++ 2-Web_Personal/webpersonal/webpersonal/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
+from core import views as core_views
+from portfolio import views as portfolio_views
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('portfolio/', views.portfolio, name="portfolio"),
-    path('contact/', views.contact, name="contact"),
-    path('about-me/', views.aboutMe, name="about-me"),
+    path('', core_views.home, name="home"),
+    path('portfolio/', portfolio_views.portfolio, name="portfolio"),
+    path('contact/', core_views.contact, name="contact"),
+    path('about-me/', core_views.aboutMe, name="about-me"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
